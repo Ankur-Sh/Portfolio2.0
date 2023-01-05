129bb9c4943a0a3b8e9a1fe93562e798acb1214b
Ankur-Sh <93051030+Ankur-Sh@users.noreply.github.com>
2023-01-05 16:13:04 +0530

    Update Acomplishments.js

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -8,10 +8,10 @@ import {
 import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles";
 
 const data = [
-    { number: 20, text: "Open Source Projects" },
-    { number: 1000, text: "Students" },
-    { number: 1900, text: "Github Followers" },
-    { number: 5000, text: "Github Stars" },
+    { number: TECHNICAL LEAD, text: "NSCC ABESEC" },
+    { number: 10+, text: "Projects" },
+    { number: 10+, text: "Github Followers" },
+    { number: 1, text: "Hactoberfest2022" },
 ];
 
 const Acomplishments = () => (
@@ -20,7 +20,7 @@ const Acomplishments = () => (
         <Boxes>
             {data.map((card, index) => (
                 <Box key={index}>
-                    <BoxNum>{card.number}+</BoxNum>
+                    <BoxText>{card.text}</BoxText>
                     <BoxText>{card.text}</BoxText>
                 </Box>
             ))}
